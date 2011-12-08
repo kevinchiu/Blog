c590f9dda38ad5583966aa3ed522a43d2960c5e0
Kevin Chiu <kevin@kevinchiu.org>
2011-12-08 03:11:02 -0800

    heroku-specific port

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+var port = process.env.PORT || 3000;
 
-app.listen(3000);
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
